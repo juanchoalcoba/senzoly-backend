90df96b9d9298331dede2fbc94cc0f2a79fd3b8c
juanchoalcoba <juanchoalcoba@gmail.com>
2026-07-26 01:19:23 -0300

    design and customers

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,26 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
 
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
   process.exit(-1);
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
-  getClient: () => pool.connect(), // Necesario para transacciones (BEGIN, COMMIT, ROLLBACK)
+  getClient: () => pool.connect(),
 };
